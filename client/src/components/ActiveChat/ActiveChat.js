2ceb4633efd4fd3a4655a11021f14f3fd28c6a85
Bhargava Guntoori <bhargava89@hotmail.com>
2022-01-30 00:48:26 -0500

    Icon appears after last read message of otherUser

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -38,6 +38,7 @@ const ActiveChat = (props) => {
               messages={conversation.messages}
               otherUser={conversation.otherUser}
               userId={user.id}
+              otherUserNumRead={conversation.otherUserNumRead}
             />
             <Input
               otherUser={conversation.otherUser}
--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,20 +1,54 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
+const useStyles = makeStyles(() => ({
+  avatar: {
+    height: 30,
+    width: 30,
+    marginRight: 11,
+    marginTop: 6
+  },
+  iconRoot: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end"
+  }
+}))
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
-  
+  const { messages, otherUser, userId, otherUserNumRead } = props;
+  const classes = useStyles()
+
+  const getIcon = (index)=>{
+    if (index == otherUserNumRead-1) {
+      return (
+      <Box className={classes.iconRoot}>
+        <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>
+      </Box>
+      )
+    } else {
+      return ''
+    }
+    
+  }
+
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, index) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <React.Fragment>
+            <SenderBubble key={message.id} text={message.text} time={time} />
+            {getIcon(index)}
+          </React.Fragment>
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <React.Fragment>
+            <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+            {getIcon(index)}
+          </React.Fragment>
         );
       })}
     </Box>
